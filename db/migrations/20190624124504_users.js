05b5a1f975860ce19b0a7e630ae1213715b82c29
janders4 <johnanderson198689@gmail.com>
2019-06-24 12:50:05 +0100

    users migration complete and working

---
+++ db/migrations/20190624124504_users.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  console.log("creating users table");
+
+  return knex.schema.createTable("users", usersTable => {
+    usersTable
+      .string("username")
+      .unique()
+      .primary()
+      .notNullable();
+    usersTable.string("avatar_url").notNullable();
+    usersTable.string("name").notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  console.log("destroying users table");
+  return knex.schema.dropTable("users");
+};
