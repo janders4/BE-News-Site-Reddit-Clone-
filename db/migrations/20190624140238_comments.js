1e48be96b1938e99781039c1f4b46d9a61eed649
janders4 <johnanderson198689@gmail.com>
2019-06-24 14:13:11 +0100

    all migrations complete

---
+++ db/migrations/20190624140238_comments.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("comments", commentsTable => {
+    commentsTable.increments("comment_id").primary();
+    commentsTable.string("author").references("users.username");
+    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.integer("votes").defaultTo(0);
+    commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
+    commentsTable.string("body", 10000).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  console.log("destroying comments table");
+  return knex.schema.dropTable("comments");
+};
