ddb39b4ea1f804b7c40968ec93a6c8353ee8b9b7
janders4 <johnanderson198689@gmail.com>
2019-06-24 17:04:11 +0100

    formatComments complete

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,12 +1,9 @@
 const formatDate = list => {
-  //const javaScriptTime = new Date(list);
-  //return javaScriptTime.toString();
   const newList = [...list];
   newList.forEach(item => {
     item["created_at"] = new Date(item["created_at"]);
     item["created_at"] = item["created_at"].toString();
   });
-  console.log(newList);
   return newList;
 };
 
@@ -14,11 +11,22 @@ const makeRefObj = list => {
   return list.length === 0
     ? {}
     : list.reduce((refs, row) => {
-        refs[row["article_id"]] = row["title"];
+        refs[row["title"]] = row["article_id"];
         return refs;
       }, {});
 };
 
-const formatComments = (comments, articleRef) => {};
+const formatComments = (comments, articleRef) => {
+  const newComments = [...comments];
+  newComments.forEach(item => {
+    item["author"] = item["created_by"];
+    item["article_id"] = item["belongs_to"];
+    delete item["created_by"];
+    delete item["belongs_to"];
+    item["article_id"] = articleRef[item["article_id"]];
+  });
+  console.log(newComments);
+  return formatDate(newComments);
+};
 
 module.exports = { formatDate, makeRefObj, formatComments };
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -33,7 +33,7 @@ describe("formatDate", () => {
   });
 });
 
-describe.only("makeRefObj", () => {
+describe("makeRefObj", () => {
   const testData = [
     {
       article_id: "1",
@@ -81,4 +81,93 @@ describe.only("makeRefObj", () => {
   });
 });
 
-describe("formatComments", () => {});
+describe.only("formatComments", () => {
+  it("returns a new array", () => {
+    const input = [];
+    expect(formatComments(input)).to.not.equal(input);
+  });
+  // it("changes the keys of a single object in an array", () => {
+  //   const testData = [
+  //     {
+  //       body:
+  //         "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+  //       belongs_to:
+  //         "The People Tracking Every Touch, Pass And Tackle in the World Cup",
+  //       created_by: "tickle122",
+  //       votes: -1,
+  //       created_at: 1468087638932
+  //     }
+  //   ];
+  //   const output = [
+  //     {
+  //       body:
+  //         "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+  //       article_id:
+  //         "The People Tracking Every Touch, Pass And Tackle in the World Cup",
+  //       author: "tickle122",
+  //       votes: -1,
+  //       created_at: "Sat Jul 09 2016 19:07:18 GMT+0100 (British Summer Time)"
+  //     }
+  //   ];
+  //   expect(formatComments(testData)).to.eql(output);
+  // });
+  // it("changes the time to javascript object", () => {
+  //   const testData = [
+  //     {
+  //       body:
+  //         "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+  //       belongs_to:
+  //         "The People Tracking Every Touch, Pass And Tackle in the World Cup",
+  //       created_by: "tickle122",
+  //       votes: -1,
+  //       created_at: 1468087638932
+  //     }
+  //   ];
+  //   const output = [
+  //     {
+  //       body:
+  //         "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+  //       article_id:
+  //         "The People Tracking Every Touch, Pass And Tackle in the World Cup",
+  //       author: "tickle122",
+  //       votes: -1,
+  //       created_at: "Sat Jul 09 2016 19:07:18 GMT+0100 (British Summer Time)"
+  //     }
+  //   ];
+  //   expect(formatComments(testData)).to.eql(output);
+  // });
+  it("article_id must be the id corresponding to the original title value provided", () => {
+    const dummyref = makeRefObj([
+      {
+        article_id: "1",
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ]);
+    const testData = [
+      {
+        body:
+          "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "tickle122",
+        votes: -1,
+        created_at: 1468087638932
+      }
+    ];
+    const output = [
+      {
+        body:
+          "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+        article_id: "1",
+        author: "tickle122",
+        votes: -1,
+        created_at: "Sat Jul 09 2016 19:07:18 GMT+0100 (British Summer Time)"
+      }
+    ];
+    expect(formatComments(testData, dummyref)).to.eql(output);
+  });
+});
