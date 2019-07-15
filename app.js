d7b19a47801614a88e22c84b63775a8527896edc
janders4 <johnanderson198689@gmail.com>
2019-07-15 14:42:09 +0100

    cors functionality added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const { apiRouter } = require("./routers/api-router");
 const { error500s } = require("./error-handling/error-500s");
 const { error400, error404 } = require("./error-handling/error-400s");
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", apiRouter);
 
 app.all("/*", (req, res, next) => {
